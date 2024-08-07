6f0bf544652527a03eef1a01045e3f8522679fa2
Ubuntu <azureuser@rag-vm.dzxlc4131d3ufb2otg1xgfyu3b.parx.internal.cloudapp.net>
2024-08-07 15:43:44 +0000

    add correction

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-pip3 install langchain_community tqdm nvidia-ml-py3
+pip install langchain_community tqdm nvidia-ml-py3
 
-BASE_URL="http://192.168.30.240:11434"
+BASE_URL="http://127.0.0.1:11434"
 OUTPUT_PATH="./output"
 
 #MODELS=( qwen2:0.5b )
-MODELS=( qwen2:0.5b qwen2:1.8b qwen2:4b qwen2:7b qwen2:7b-chat-q5_0 qwen2:7b-chat-q6_K qwen2:7b-chat-q8_0 qwen2:7b-chat-fp16 qwen2:14b qwen2:32B qwen2:72B qwen2:110B )
+#MODELS=( qwen2:0.5b qwen2:1.5b qwen2:7b qwen2:72b )
+MODELS=( gemma2:2b gemma2:9b gemma2:27b )
 
 
 # Force model download to avoid too long delays between experiments
@@ -18,6 +19,6 @@ MODELS=( qwen2:0.5b qwen2:1.8b qwen2:4b qwen2:7b qwen2:7b-chat-q5_0 qwen2:7b-cha
 for MODEL in "${MODELS[@]}"; do
    curl -X POST $BASE_URL/api/pull -d '{"name": "'"${MODEL}"'"}'
    echo "Running script for model ${MODEL}"
-   python token_rate.py --base_url $BASE_URL --model=$MODEL --output=$OUTPUT_PATH --loops=1
+   python token_rate.py --base_url $BASE_URL --model=$MODEL --output=$OUTPUT_PATH --loops=17
    curl -X DELETE $BASE_URL/api/delete -d '{"name": "'"${MODEL}"'"}'
 done
