1f261728e0f7349d85a981cc3b931d83bec8e873
Loïc VEYSSIERE <loic.veyssiere@meritis.fr>
2024-08-07 15:10:21 +0200

    remove model at the end to avoid disk saturation

--- run.sh
+++ run.sh
@@ -10,12 +10,14 @@ MODELS=( qwen2:0.5b qwen2:1.8b qwen2:4b qwen2:7b qwen2:7b-chat-q5_0 qwen2:7b-cha
 
 
 # Force model download to avoid too long delays between experiments
-for MODEL in "${MODELS[@]}"; do
-   curl $BASE_URL/api/pull -d '{"name": "'"${MODEL}"'"}'
-done
+#for MODEL in "${MODELS[@]}"; do
+#   curl $BASE_URL/api/pull -d '{"name": "'"${MODEL}"'"}'
+#done
 
 
 for MODEL in "${MODELS[@]}"; do
+   curl -X POST $BASE_URL/api/pull -d '{"name": "'"${MODEL}"'"}'
    echo "Running script for model ${MODEL}"
    python token_rate.py --base_url $BASE_URL --model=$MODEL --output=$OUTPUT_PATH --loops=1
+   curl -X DELETE $BASE_URL/api/delete -d '{"name": "'"${MODEL}"'"}'
 done
