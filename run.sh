5b62f795c22e11bdb2e3f8f3fbfd525d62cef848
Loïc VEYSSIERE <loic.veyssiere@meritis.fr>
2024-08-08 11:27:47 +0200

    correction on phi3

--- run.sh
+++ run.sh
@@ -10,8 +10,9 @@ BASE_URL="http://127.0.0.1:11434"
 OUTPUT_PATH="./output"
 
 #MODELS=( qwen2:0.5b )
-#MODELS=( qwen2:0.5b qwen2:1.5b gemma2:2b phi3:3b mistral:7b qwen2:7b llama3.1:8b gemma2:9b mistral-nemo:12b phi3:14b gemma2:27b llama3.1:70b qwen2:72b mistral-large:123b )
-MODELS=( gemma2:2b gemma2:9b gemma2:27b )
+#MODELS=( gemma2:2b gemma2:9b gemma2:27b )
+#MODELS=( qwen2:0.5b qwen2:1.5b gemma2:2b phi3:3.8b mistral:7b qwen2:7b llama3.1:8b gemma2:9b mistral-nemo:12b phi3:14b gemma2:27b llama3.1:70b qwen2:72b mistral-large:123b )
+
 
 
 # Force model download to avoid too long delays between experiments
